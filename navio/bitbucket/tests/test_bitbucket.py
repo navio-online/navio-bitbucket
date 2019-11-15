e63ab3e573f84e6306b78ab8a66bb560e3cd157a
Peter Salnikov <peter@salnikov.cc>
2019-11-15 09:29:27 +0300

    fix test

--- navio/bitbucket/tests/test_bitbucket.py
+++ navio/bitbucket/tests/test_bitbucket.py
@@ -7,67 +7,67 @@ class TestImport:
 
     def test_import(self):
         import navio.bitbucket
-        from navio.bitbucket import Travis
+        from navio.bitbucket import Bitbucket
 
 
 class Test:
 
     def test_is_bitbucket(self):
-        from navio.bitbucket import Travis
+        from navio.bitbucket import Bitbucket
 
         os.environ.pop('CI', None)
-        assert not Travis().is_bitbucket()
+        assert not Bitbucket().is_bitbucket()
 
         os.environ['CI'] = 'true'
-        assert Travis().is_bitbucket()
+        assert Bitbucket().is_bitbucket()
 
         os.environ['CI'] = 'false'
-        assert not Travis().is_bitbucket()
+        assert not Bitbucket().is_bitbucket()
 
         os.environ['CI'] = '???'
-        assert not Travis().is_bitbucket()
+        assert not Bitbucket().is_bitbucket()
 
     def test_is_pull_request(self):
-        from navio.bitbucket import Travis
+        from navio.bitbucket import Bitbucket
 
         os.environ.pop('BITBUCKET_PR_ID', None)
-        assert not Travis().is_pull_request()
+        assert not Bitbucket().is_pull_request()
 
         os.environ['CI'] = 'true'
 
         os.environ['BITBUCKET_PR_ID'] = 'true'
-        assert Travis().is_pull_request()
+        assert Bitbucket().is_pull_request()
 
         os.environ['BITBUCKET_PR_ID'] = 'false'
-        assert not Travis().is_pull_request()
+        assert not Bitbucket().is_pull_request()
 
         os.environ['BITBUCKET_PR_ID'] = '???'
-        assert not Travis().is_pull_request()
+        assert not Bitbucket().is_pull_request()
 
     def test_branch(self):
-        from navio.bitbucket import Travis
+        from navio.bitbucket import Bitbucket
 
         os.environ['CI'] = 'true'
         os.environ.pop('BITBUCKET_BRANCH', None)
 
-        assert Travis().branch() is None
+        assert Bitbucket().branch() is None
 
         os.environ['BITBUCKET_BRANCH'] = 'master'
-        assert 'master' == Travis().branch()
+        assert 'master' == Bitbucket().branch()
 
         os.environ['BITBUCKET_BRANCH'] = 'prod'
-        assert 'prod' == Travis().branch()
+        assert 'prod' == Bitbucket().branch()
 
     def test_commit_hash(self):
-        from navio.bitbucket import Travis
+        from navio.bitbucket import Bitbucket
 
         os.environ['CI'] = 'true'
         os.environ.pop('BITBUCKET_COMMIT', None)
 
-        assert '000000000000000000000000000000' == Travis().commit_hash()
+        assert '000000000000000000000000000000' == Bitbucket().commit_hash()
 
         os.environ['BITBUCKET_COMMIT'] = '1f510ab451bb4'
-        assert '1f510ab451bb4' == Travis().commit_hash()
+        assert '1f510ab451bb4' == Bitbucket().commit_hash()
 
         os.environ['BITBUCKET_COMMIT'] = '04124124bcb131'
-        assert '04124124bcb131' == Travis().commit_hash()
+        assert '04124124bcb131' == Bitbucket().commit_hash()
