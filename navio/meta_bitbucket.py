9e087cf255723d9c5ed96b4bf5cf78c6c052eb7c
Peter Salnikov <peter@salnikov.cc>
2020-07-08 01:51:36 +0300

    Version updated to 0.2.2

--- navio/meta_bitbucket.py
+++ navio/meta_bitbucket.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __license__ = "Apache 2.0 license"
 __website__ = "https://oss.navio.online/navio-bitbucket/"
 __download_url__ = ('https://github.com/navio-online/navio-bitbucket/archive'
